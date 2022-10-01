1cd2c5b2ca3ac362aeb8c70d54b0c264d8068ff5
danofsteel32 <dan@dandavis.dev>
2022-10-01 16:19:09 -0400

    wip

--- src/quart_sse_demo/server.py
+++ src/quart_sse_demo/server.py
@@ -1,77 +1,12 @@
 import asyncio
-from dataclasses import dataclass, field
+from dataclasses import dataclass
 
 from quart import Quart, abort, jsonify, make_response, render_template, request
 from quart.helpers import stream_with_context
 
+from .clients import ChatClient, ConnectedClients
+
 app = Quart(__name__)
-
-QUEUE_SIZE = 50
-
-
-def _get_queue():
-    return asyncio.Queue(QUEUE_SIZE)
-
-
-@dataclass
-class ChatClient:
-    username: str
-    status: str = "Online"
-    queue: asyncio.Queue[dict[str, str]] = field(default_factory=_get_queue)
-
-
-class ConnectedClients:
-    def __init__(self):
-        self._clients: dict[str, ChatClient] = {}
-
-    def __setitem__(self, username: str, client: ChatClient):
-        self._clients[username] = client
-
-    def __getitem__(self, username: str) -> ChatClient | None:
-        try:
-            return self._clients[username]
-        except KeyError:
-            return None
-
-    def __delitem__(self, username: str):
-        try:
-            del self._clients[username]
-        except KeyError:
-            pass
-
-    def __iter__(self):
-        for username in self._clients:
-            yield self._clients[username]
-
-    async def update_status(self, username: str, status: str) -> bool:
-        try:
-            self._clients[username].status = status
-        except KeyError:
-            print(self._clients)
-            return False
-
-        status_update = {"type": "status_update", "sender": username, "content": status}
-        app.logger.info(status_update)
-        for client in self._clients:
-            if self._clients[client].username == username:
-                continue
-            await self._clients[client].queue.put(status_update)
-        return True
-
-    async def new_message(self, username: str, message: str) -> bool:
-        try:
-            self._clients[username]
-        except KeyError:
-            return False
-
-        _message = {"type": "message", "sender": username, "content": message}
-        for client in self._clients:
-            await self._clients[client].queue.put(_message)
-        return True
-
-
-# @app.before_serving
-# async def init_clients():
 app.clients = ConnectedClients()
 
 
@@ -101,7 +36,9 @@ class ServerSentEvent:
 async def get_event(data: dict) -> ServerSentEvent:
     if data["type"] == "status_update":
         status, username = data["content"], data["sender"]
-        html = await render_template("status_partial.jinja", status=status, username=username)
+        html = await render_template(
+            "status_partial.jinja", status=status, username=username
+        )
         event = ServerSentEvent(html, event="status_update")
     elif data["type"] == "message":
         message, sender = data["content"], data["sender"]
@@ -125,7 +62,7 @@ async def index(username: str):
 @app.route("/<username>/status", methods=["PUT"])
 async def update_status(username: str):
     data = await request.get_json()
-    updated = await app.clients.update_status(username, data["status"])
+    updated, status_update = await app.clients.update_status(username, data["status"])
     if updated:
         return f'Chatting as {username}, Status: {data["status"]}'
     return jsonify(updated)
@@ -134,9 +71,9 @@ async def update_status(username: str):
 @app.route("/<username>/message", methods=["PUT"])
 async def message(username: str):
     data = await request.get_json()
-    updated = await app.clients.new_message(username, data["message"])
-    if updated:
-        return jsonify(updated)
+    updated, msg = await app.clients.new_message(username, data["message"])
+    app.logger.info(updated, msg)
+    return jsonify(updated)
 
 
 @app.route("/sse")
@@ -150,6 +87,8 @@ async def sse():
         abort(400)
 
     app.clients[username] = ChatClient(username)
+    app.clients.update_status(username, "Online")
+    app.logger.info(f"Add client {username}")
 
     # decorator needed to call render_template()
     @stream_with_context
@@ -164,6 +103,9 @@ async def sse():
                 app.logger.info("Removing Client")
                 del app.clients[username]
                 break
+            except RuntimeError:
+                print("HERE")
+                continue
 
     headers = {
         "Content-Type": "text/event-stream",
